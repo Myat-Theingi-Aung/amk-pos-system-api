1cc53bfc7aeea45c13d8206c5b7d4acb33fa006a
lwinleo05 <yinmyatnoelwin1@gmail.com>
2023-08-15 08:40:42 +0630

    Category crud

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryCreateRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryCreateRequest $request)
+    {
+        $category = Category::create($request->all());
+
+        return response()->json(['message' => 'Category created successfully','category' =>  new CategoryResource($category)]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryUpdateRequest $request, Category $category)
+    {
+        $category->update($request->all());
+
+        return response()->json(['message' => 'Category updated successfully', 'category' => new CategoryResource($category) ]);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        
+        return response()->json(['message' => 'Category deleted successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [UserController::class, 'store']);
@@ -12,5 +13,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/{user}', [ UserController::class, 'show' ]);
     Route::put('/user/{user}', [ UserController::class, 'update' ]);
     Route::delete('/user/{user}', [ UserController::class, 'destroy' ]);
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
