4ee3d94fb40f7e122cd5687c64f57a63cecbb922
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-08-18 11:57:46 +0630

    update payment table migration

--- database/migrations/2023_08_14_044819_create_payments_table.php
+++ database/migrations/2023_08_14_044819_create_payments_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category_type_id');
             $table->integer('amount');
             $table->date('date');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_type_id')->references('id')->on('category_types')->onDelete('cascade');
             $table->timestamps();
         });
     }
