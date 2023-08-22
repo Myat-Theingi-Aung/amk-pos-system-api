ce0039fbcce7f5153b80b2cabc649d24a8dc1560
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-08-22 11:42:22 +0630

    add reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ResetPasswordRequest;
 
 
 class ResetPasswordController extends Controller
 {
-    public function reset(ResetPasswordRequest $request, User $user){
+    public function reset(ResetPasswordRequest $request, User $user)
+    {
+        if ($user && Carbon::parse($user->token_expiry)->gt(now())) {
+            $user->password = Hash::make($request->new_password);
+            $user->reset_token = null;
+            $user->token_expiry = null;
+            $user->save();
+    
+            return response()->json(['message' => 'Password reset successfully'], 200);
+        } 
         
+        return response()->json(['message' => 'Invalid token or token expired'], 400);
     }
 }
--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ResetPasswordRequest extends FormRequest
@@ -22,7 +23,7 @@ class ResetPasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'new_password' => ['required','string',Password::min(5)->mixedCase()->numbers()->symbols()->uncompromised(),'confirmed']
         ];
     }
 }
